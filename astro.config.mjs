b3ceb4a03914441c8c5a9660a9e8b27e03644d13
vtmworld <vtmworld@gmail.com>
2024-12-27 23:34:05 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,17 @@ import { defineConfig } from 'astro/config';
 
 //import { defineConfig } from 'astro/config';
 
-import { defineConfig } from 'astro/config';
+//import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://vtmworld.github.io/astroneww',
-  output: 'static', // Required for GitHub Pages
-  base: '/astroneww/', // Replace with your repository name
+  base: '/astroneww/',
+  output: 'static',
 });
 
 
 
 
 
-import { defineConfig } from 'astro/config';
+//import { defineConfig } from 'astro/config';
 
