114a68b3d24f981bed387f8a32a1b70b234d1ad3
vtmworld <vtmworld@gmail.com>
2024-12-27 23:12:08 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-//export default defineConfig({});
+export default defineConfig({});
 
-export default defineConfig({
-  trailingSlash: false, // Ensures clean URLs
-  build: {
-    format: 'directory' // Ensures directories instead of index.html
-  }
-});
+
 
 import { defineConfig } from 'astro/config';
 
